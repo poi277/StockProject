1fb6256140ee3074b133d1a10392cd15ec3d6910
poi277@naver.com <poi277@naver.com>
2026-03-10 22:18:03 +0900

    update

--- StockBackEnd/src/main/java/Poi/Stock/config/SecurityConfig.java
+++ StockBackEnd/src/main/java/Poi/Stock/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
 
 				.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 						.requestMatchers("/", "/hello", "/user/register", "/stock/**", "/ws/**", "/auth/**",
-								"/order/orderbook/*")
+								"/order/orderbook/*", "/api/kis/stock/**")
 						.permitAll()
 						.anyRequest().authenticated())
 
